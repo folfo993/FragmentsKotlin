8b8cd7c0633071eff79615488f1f4db3e0ebd70b
FOLFO93 <uv.adolfo@hotmail.com>
2018-05-14 12:18:40 +0200

    Primer commit. Creacion del primer Fragment

---
+++ app/src/main/java/com/example/folfo93/fragmentskotline/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.example.folfo93.fragmentskotline
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.jetbrains.anko.support.v4.toast
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [MainFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_main, container)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        main_button.setOnClickListener {
+            toast("Mensaje: ${main_editText.text}")
+            main_textview.text = main_editText.text
+        }
+    }
+}
